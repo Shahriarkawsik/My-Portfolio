51257508f566c381c6b0a635a8070c622ece4083
Md. Shahriar Kabir <shahriarkawsik@gmail.com>
2025-02-07 01:12:05 +0600

    add skill section

---
+++ src/Components/Home/Skills.jsx
@@ -0,0 +1,36 @@
+import { skills } from "../../JavaScript/skills";
+
+const Skills = () => {
+  return (
+    <section className=" font-Raleway w-11/12 lg:w-4/5 mx-auto lg:space-y-20">
+      <div className="space-y-6">
+        <h1 className="text-4xl leading-10 text-color1 font-bold border-b-4 border-pink-600 inline ">
+          Skills
+        </h1>
+        <p className="text-color5 mt-5">
+          These are the technologies Ive worked with
+        </p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 items-center justify-center">
+        {/* Html */}
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className="flex flex-col justify-center items-center gap-2 rounded-xl shadow-[0px_4px_6px_-1px_rgb(4,12,22),0px_2px_4px_-2px_rgb(4,12,22)] p-5"
+          >
+            <figure className="w-20 h-20">
+              <img
+                className="w-full h-full object-center "
+                src={skill?.logo}
+                alt=""
+              />
+            </figure>
+            <h1 className="text-color1 leading-6">{skill?.skill}</h1>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Components/Header/Banner";
+import Skills from "../Components/Home/Skills";
 
 const Home = () => {
   return (
     <div className="">
       <Banner />
+      <Skills />
     </div>
   );
 };
